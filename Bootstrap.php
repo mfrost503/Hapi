4ed7edd59d2fa5e19f82967c0d4ba3bf09227fb3
Matt Frost <mfrost.design@gmail.com>
2013-03-02 12:58:14 -0600

    starting over on the tests

--- Bootstrap.php
+++
@@ -1,3 +0,0 @@
-<?php
-set_include_path(get_include_path() . PATH_SEPARATOR . dirname(__DIR__).'/src/');
-include 'vendor/autoload.php';
---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+namespace IceBird\Twitter;
+require_once 'vendor/autoload.php';
+$consumer = new OAuthConsumer('123','123');
